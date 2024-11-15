f99b44ec4ab6492cd05e5d109a3e606efcdf1984
Abdul-Hadi06 <ahadi.khu007@gmail.com>
2024-11-15 23:41:23 +0400

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,9 @@
+def hello():
+    print("Hello")  # Displays "Hello" as an output in the console.
+
+def main():
+    hello()  # The hello() function is called in order to run it.
+
+# This line verifies whether this script is being executed instantly.
+if __name__ == "__main__":
+    main()  # If the script is executed instantly, it launches the main() function.
