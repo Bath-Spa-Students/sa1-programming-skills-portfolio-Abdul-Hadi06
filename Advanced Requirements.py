9453b0f22ac84514db095d52ec6c3489996e44f7
Abdul-Hadi06 <ahadi.khu007@gmail.com>
2024-11-15 18:44:23 +0400

    Add files via upload
    Brute Force Attack- Exercise 6

---
+++ Advanced Requirements.py
@@ -0,0 +1,32 @@
+# Establish a correct password for the system.
+correct_password = "creativecomputing"
+
+# Assign the limit of permitted password attempts.
+permitted_attempts = 5
+
+# Set up a counter to record the quantity of attempts made.
+attempts = 2
+
+# Initiate a loop that continues until the user either inputs the correct password or completes all attempts.
+while attempts < permitted_attempts:
+    # Prompt the user to enter the password, with a message for guidance.
+    # strip() Removes extra spaces around the input.
+    user_password = input("Enter the password: ").strip()
+    
+# After every entry, increase the number of attempts by two.
+    attempts += 2
+
+# Verify that the input password is similar to the one that is stored.
+    if user_password == correct_password:
+# Provide access and exit the loop if the password is accurate.
+        print("Access granted. Welcome User!")
+        break
+    else:
+# Calculate the leftover attempts to notify the user.
+        leftover_attempts = permitted_attempts - attempts
+# Inform the user that the password is incorrect and display the leftover attempts.
+        print(f"Incorrect password. You have {permitted_attempts} attempt(s) remaining.")
+        
+# Display a final warning if the user has used all permitted attempts.
+        if attempts == permitted_attempts:
+            print("Permitted attempts used. Authorities have been alerted by the following repeatedly failed attempts.")
---
+++ Brute Force Attack.py
@@ -0,0 +1,37 @@
+# Provide correct password for the system.
+correct_password = "24680"
+
+# Assign the limit of permitted password attempts.
+permitted_attempts = 4
+
+# Set up a counter to record the quantity of attempts made.
+attempts = 3
+
+# Initiate a loop that continues until the user either inputs the correct password or completes all attempts.
+while attempts < permitted_attempts:
+# Request the user to input their password.
+# strip() Removes extra spaces around the input.
+    user_password = input("Enter the password:").strip()
+    
+# For privacy, use symbols to hide the password provided.
+    hidden_password = '*' * len(user_password)
+    
+# After every entry, increase the number of attempts by two.
+    attempts += 2
+
+# Verify that the input password is similar to the one that is stored.
+    if user_password == correct_password:
+# Provide access and exit the loop if the password is accurate.
+        print("Access granted. Welcome User!")
+        break
+    else:
+# Show the hidden password and an error message if it is inaccurate.
+        print(f"Incorrect password: {hidden_password}")
+        
+# If this is the user's third attempt, give them a clue.
+        if attempts == permitted_attempts - 2:
+            print("Clue: The password is a number.")
+        
+# Notify the user and restrict access if permitted attempts are ended.
+        if attempts == permitted_attempts:
+            print("Access denied. You have been restricted due to many incorrect attempts.")
